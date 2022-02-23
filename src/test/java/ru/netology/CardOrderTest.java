e04d764c34596d6319092fca56d5548acb909118
Markvitim <markvitim@gmail.com>
2022-02-23 22:31:07 +0300

    appveyor add project

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -2,7 +2,7 @@ package ru.netology;
 
 
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Test;
+ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
