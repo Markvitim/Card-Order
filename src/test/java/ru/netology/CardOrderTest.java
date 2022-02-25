ddb02babf3072870de261b2d4cb3430e736f253b
Markvitim <markvitim@gmail.com>
2022-02-26 01:48:55 +0300

    tests added

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,13 +1,12 @@
 package ru.netology;
 
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
- import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
 
@@ -20,12 +19,12 @@ public class CardOrderTest {
         form.$(By.cssSelector("[data-test-id='phone'] input")).setValue("+79253004455");
         form.$(By.cssSelector(".checkbox__box")).click();
         form.$(By.cssSelector("button")).click();
-        String actual = $(By.className("Success_successBlock__2L3Cw")).getText();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        assertEquals(expected, actual.trim());
+        $(By.cssSelector("[data-test-id='order-success']")).shouldHave(Condition.text("Ваша заявка успешно отправлена! " +
+                "Наш менеджер свяжется с вами в ближайшее время"));
     }
+
     @Test
-    public void shouldCheckName() {
+    public void shouldCheckNameLatin() {
         open("http://localhost:9999");
 
         SelenideElement form = $("form");
@@ -33,21 +32,68 @@ public class CardOrderTest {
         form.$(By.cssSelector("[data-test-id='phone'] input")).setValue("+79253004455");
         form.$(By.cssSelector(".checkbox__box")).click();
         form.$(By.cssSelector("button")).click();
-        String actual = $(By.cssSelector(".input_type_text .input__sub")).getText();
-        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        assertEquals(expected, actual.trim());
+        $(By.cssSelector("[data-test-id=\"name\"] .input_invalid, .input__sub")).shouldHave(Condition.text("Допустимы только русские " +
+                "буквы, пробелы"));
     }
+
     @Test
-    public void shouldCheckPhone() {
+    public void shouldCheckNameEmpty() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$(By.cssSelector("[data-test-id='name'] input")).setValue("");
+        form.$(By.cssSelector("[data-test-id='phone'] input")).setValue("+79253004455");
+        form.$(By.cssSelector(".checkbox__box")).click();
+        form.$(By.cssSelector("button")).click();
+        $(By.cssSelector("[data-test-id=\"name\"] .input_invalid, .input__sub")).shouldHave(Condition.text("обязательно для заполнения"));
+    }
+
+
+    @Test
+    public void shouldCheckPhoneEmpty() {
         open("http://localhost:9999");
 
         SelenideElement form = $("form");
         form.$(By.cssSelector("[data-test-id='name'] input")).setValue("Василий Иванов-Петров");
-        form.$(By.cssSelector("[data-test-id='phone'] input")).setValue(" ");
+        form.$(By.cssSelector("[data-test-id='phone'] input")).setValue("");
         form.$(By.cssSelector(".checkbox__box")).click();
         form.$(By.cssSelector("button")).click();
-        String actual = $(By.cssSelector(".input_type_tel  .input__sub")).getText();
-        String expected = "Поле обязательно для заполнения";
-        assertEquals(expected, actual.trim());
+        $(By.cssSelector("[data-test-id=\"phone\"].input_invalid,.input_type_tel ")).should(Condition.text("обязательно для заполнения"));
     }
+
+    @Test
+    public void shouldCheckPhoneEmptyVar() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$(By.cssSelector("[data-test-id='name'] input")).setValue("Василий Иванов-Петров");
+        form.$(By.cssSelector("[data-test-id='phone'] input")).setValue("");
+        form.$(By.cssSelector(".checkbox__box")).click();
+        form.$(By.cssSelector("button")).click();
+        $$(By.cssSelector("[data-test-id=\"phone\"].input_invalid,.input__sub ")).last().should(Condition.text("обязательно для заполнения"));
+    }
+
+    @Test
+    public void shouldCheckPhoneNotFull() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$(By.cssSelector("[data-test-id='name'] input")).setValue("Василий Иванов-Петров");
+        form.$(By.cssSelector("[data-test-id='phone'] input")).setValue("+7 ");
+        form.$(By.cssSelector(".checkbox__box")).click();
+        form.$(By.cssSelector("button")).click();
+        $$(By.cssSelector("[data-test-id=\"phone\"].input_invalid,.input__sub ")).last().shouldHave(Condition.text("Должно быть 11 цифр"));
+    }
+
+    @Test
+    public void shouldCheckBox() {
+        open("http://localhost:9999");
+
+        SelenideElement form = $("form");
+        form.$(By.cssSelector("[data-test-id='name'] input")).setValue("Василий Иванов-Петров");
+        form.$(By.cssSelector("[data-test-id='phone'] input")).setValue("+78985236547");
+        form.$(By.cssSelector("button")).click();
+        $(By.cssSelector("[role=\"presentation\"]")).should(Condition.text("соглашаюсь с условиями обработки"));
+    }
+
 }
